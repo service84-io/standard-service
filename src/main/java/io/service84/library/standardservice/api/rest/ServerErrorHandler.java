ecea4db049fd599aa502796d73a57947d9e77925
Tyler <Tyler@service84.io>
2021-02-11 08:33:26 -0500

    Cleanup ServerErrorHandler
    Remove unused HttpServletRequest
    Add a handler for RuntimeExceptions

--- src/main/java/io/service84/library/standardservice/api/rest/ServerErrorHandler.java
+++ src/main/java/io/service84/library/standardservice/api/rest/ServerErrorHandler.java
@@ -1,7 +1,5 @@
 package io.service84.library.standardservice.api.rest;
 
-import javax.servlet.http.HttpServletRequest;
-
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -16,10 +14,17 @@ public class ServerErrorHandler {
 
   @ExceptionHandler(Error.class)
   @ResponseBody
-  public ResponseEntity<Object> handleExceptionalResultException(
-      HttpServletRequest request, Error ex) {
+  public ResponseEntity<ErrorDTO> handleUncaughtError(Error e) {
     ErrorDTO dto = new ErrorDTO();
-    dto.message = ex.getMessage();
+    dto.message = e.getMessage();
+    return new ResponseEntity<>(dto, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+
+  @ExceptionHandler(RuntimeException.class)
+  @ResponseBody
+  public ResponseEntity<ErrorDTO> handleUncaughtRuntimeException(RuntimeException e) {
+    ErrorDTO dto = new ErrorDTO();
+    dto.message = e.getMessage();
     return new ResponseEntity<>(dto, HttpStatus.INTERNAL_SERVER_ERROR);
   }
 }
