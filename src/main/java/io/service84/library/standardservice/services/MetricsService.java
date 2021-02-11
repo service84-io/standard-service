0ae22988112efb7904439147035c1fa63706b749
Tyler <Tyler@service84.io>
2021-02-11 17:11:56 -0500

    Wrap the MetricsContributor calls in a catch all

--- src/main/java/io/service84/library/standardservice/services/MetricsService.java
+++ src/main/java/io/service84/library/standardservice/services/MetricsService.java
@@ -10,9 +10,9 @@ import org.springframework.stereotype.Service;
 @Service("CB8BAAA5-D6BD-46A0-91AE-D9C076F96598")
 public class MetricsService {
   public static interface MetricsContributor {
-    Object value();
-
     String key();
+
+    Object value();
   }
 
   List<MetricsContributor> metricsContributors = new CopyOnWriteArrayList<>();
@@ -22,7 +22,11 @@ public class MetricsService {
 
     for (MetricsContributor metricsContributor : metricsContributors) {
       if (metricsContributor != null) {
-        metrics.put(metricsContributor.key(), metricsContributor.value());
+        try {
+          metrics.put(metricsContributor.key(), safeValue(metricsContributor));
+        } catch (Throwable t) {
+          // Bad MetricsContributor
+        }
       }
     }
 
@@ -31,6 +35,14 @@ public class MetricsService {
 
   public synchronized void registerMetricsContributor(MetricsContributor metricsContributor) {
     metricsContributors.add(metricsContributor);
+  }
+
+  private Object safeValue(MetricsContributor metricsContributor) {
+    try {
+      return metricsContributor.value();
+    } catch (Throwable t) {
+      return null;
+    }
   }
 
   public synchronized void unregisterReadinessContributor(MetricsContributor metricsContributor) {
