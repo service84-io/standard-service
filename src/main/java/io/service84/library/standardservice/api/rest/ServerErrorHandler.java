56fe8636ff3efe845ca5f3d551eb33b00d9b1edc
Tyler <Tyler@service84.io>
2022-04-11 00:17:43 +0000

    Add getter and setter to synthetic error dto

--- src/main/java/io/service84/library/standardservice/api/rest/ServerErrorHandler.java
+++ src/main/java/io/service84/library/standardservice/api/rest/ServerErrorHandler.java
@@ -28,6 +28,14 @@ public class ServerErrorHandler {
 
   public static class ErrorDTO {
     public String message;
+
+    public String getMessage() {
+      return message;
+    }
+
+    public void setMessage(String message) {
+      this.message = message;
+    }
   }
 
   @ExceptionHandler(Error.class)
